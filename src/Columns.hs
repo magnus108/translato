e287e050c6e58d8e9b2226a54e46eec5401992d3
Magnus Møller <magnus108@me.com>
2021-04-08 10:40:47 +0200

    correct dont know how

--- src/Columns.hs
+++ src/Columns.hs
@@ -28,6 +28,9 @@ gg = fmap . foldFix $ \x -> case x of
         Column' xs -> [UI.div #. "column" #+ xs]
         Columns' ys -> fmap (\y -> UI.div #. "columns" #+ y) ys
 
+grid :: [Columns] -> [UI Element]
+grid columns = (\(Columns cols) -> UI.div #. "columns" #+ ((\(Column col) -> UI.div #. "column" #+ col) <$> cols)) <$> columns
+
 
 
 data Single a
@@ -43,7 +46,7 @@ data Item a
     | Multiple' (Multiple a)
     deriving Functor
 
-data Items a = Items [Item a]
+data Items a = Items [[Item a]]
     deriving Functor
 
 type Grid = Fix Items
@@ -55,27 +58,38 @@ lola2 = Fix $ Items
         -- [ Single UI.div, Single UI.div, Single UI.div]
         --[ Multiple [Single (UI.string "bob"), Single (UI.string "bob2")],Single (UI.string "bob") , Nested $ Fix $ Items [ Single (UI.string "bob2")]]
    --     [ Multiple [Multiple [ Single (UI.string "bob"), Single (UI.string "bob2")]],Single (UI.string "bob") , Nested $ Fix $ Items [ Single (UI.string "bob2")]]
-        [Multiple' $ Multiple [Nested $ Fix $ Items [Single' $ Single $ UI.string "lols"],Single (UI.string "bob2"), Single (UI.string "bob")], Single' $ Single $ UI.string "baba"]
+        --[[Multiple' $ Multiple [Nested $ Fix $ Items [[Single' $ Single $ UI.string "lols"]],Single (UI.string "bob2"), Single (UI.string "bob")], Single' $ Single $ UI.string "baba"]]
+        [ [Single' $ Nested $ Fix $ Items [[Single' $ Single $ UI.string "lols"]]]
+        , [Single' $ Nested $ Fix $ Items [[Single' $ Single $ UI.string "lols"]]]
+        , [Single' $ Single $ UI.string "lols", Single' $ Single $ UI.string "lols2"]
+        , [Single' $ Single $ UI.string "lols", Multiple' $ Multiple [Single $ UI.string "lols", Single $ UI.string "lols2"]]
+        , [Single' $ Single $ UI.string "lols", Multiple' $ Multiple [Nested $ Fix $ Items [[Single' $ Single $ UI.string "lols"], [Single' $ Single $ UI.string "lols"],[Single' $ Single $ UI.string "lols"]],  Single $ UI.string "lols", Single $ UI.string "lols2"]]
+        ]
 
 
 
-grid5 :: Single (UI Element) -> UI Element
-grid5 x = case x of
+grid8 :: Single [UI Element] -> UI Element
+grid8 x = case x of
+            Single x -> UI.div #. "column" #+ [x]
+            Nested z -> UI.div #. "column" #+ z
+
+
+grid9 :: Single [UI Element] -> UI Element
+grid9 x = case x of
             Single x -> x
-            Nested z -> z
+            Nested z -> UI.div #. "columns" #+ [UI.div #. "column" #+ z]
 
 
-grid3 :: Item (UI Element) -> UI Element
-grid3 x = case x of
-            (Single' y) -> UI.div #. "column" #+ [grid5 y]
-            (Multiple' (Multiple ys)) -> UI.div #. "column" #+ (fmap grid5 ys)
+grid7 :: Item [UI Element] -> UI Element
+grid7  x = case x of
+            (Single' y) -> grid8 y
+            (Multiple' (Multiple ys)) -> UI.div #. "column" #+ fmap grid9 ys
 
-makeIt :: Items (UI Element) -> UI Element
-makeIt (Items xs) = UI.div #. "columns" #+ (fmap (\x -> grid3 x) xs)
 
-grid2 :: Grid -> UI Element
+makeIt :: Items [UI Element] -> [UI Element]
+makeIt (Items xss) = fmap (\xs -> UI.div #. "columns" #+ fmap grid7 xs) xss 
+
+grid2 :: Grid -> [UI Element]
 grid2 = foldFix $ makeIt
 
 
-grid :: [Columns] -> [UI Element]
-grid columns = (\(Columns cols) -> UI.div #. "columns" #+ ((\(Column col) -> UI.div #. "column" #+ col) <$> cols)) <$> columns
