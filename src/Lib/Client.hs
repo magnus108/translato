6b3e4096d3e465f7b41a11f2adcb065059c9a0cc
Magnus Møller <magnus108@me.com>
2021-05-26 18:34:04 +0200

    fuck stop mig

--- src/Lib/Client.hs
+++ src/Lib/Client.hs
@@ -39,48 +39,105 @@ import qualified Control.Monad.Except          as E
 
 import           Lib.Utils
 import           Lib.Api.Types
-import           Lib.Api                 hiding ( GetPhotographers, GetTabs)
+import           Lib.Api                 hiding ( GetPhotographers
+                                                , GetTabs
+                                                )
 import qualified Utils.ListZipper              as ListZipper
 import qualified Control.Lens                  as Lens
-import qualified Lib.Data.Photographer as Photographer
+import qualified Lib.Data.Photographer         as Photographer
+import qualified Lib.Data.Tab                  as Tab
+import           Control.Comonad
+
+
+mkTabs = do
+    (BTabs bTabs) <- grab @BTabs
+    liftUI $ UI.div # sink
+        items
+        ((fromMaybe []) <$> (fmap (ListZipper.toList . Tab.unTabs)) <$> bTabs)
+
+mkToken = do
+    (BToken bToken) <- grab @BToken
+    liftUI $ UI.p # sink
+        text
+        (show <$> (fromMaybe (B.empty)) <$> (fmap setCookieValue) <$> bToken)
+
+
+mkContent :: ClientApp Element
+mkContent = do
+    (BTabs bTabs)        <- grab @BTabs
+
+    photographersContent <- liftUI $ UI.string "fuck"
+    elseContent          <- liftUI $ UI.string "fuck2"
+    elseContent2         <- liftUI $ UI.string "fuck2nodATA"
+
+    let contents =
+            fmap
+                    (\xs ->
+                        let focus = extract (Tab.unTabs xs)
+                        in  case focus of
+                                Tab.PhotographersTab -> [photographersContent]
+                                _                    -> [elseContent]
+                    )
+                <$> bTabs
+
+    liftUI $ UI.div # sink children (fromMaybe [elseContent2] <$> contents)
+
+
+
 
 setup :: Window -> ClientApp ()
 setup win = do
     liftUI $ return win # set title "test"
 
 
-    _               <- loginClient
-    _               <- getPhotographersClient
-    _               <- getTabsClient
+    _                               <- loginClient
+    _                               <- getPhotographersClient
+    _                               <- getTabsClient
 
+    elemTabs                        <- mkTabs
+    elemToken                       <- mkToken
+    elemContent                     <- mkContent
 
-    (BToken bToken) <- grab @BToken
-    elem            <- liftUI $ UI.p # sink
-        text
-        (show <$> (fromMaybe (B.empty)) <$> (fmap setCookieValue) <$> bToken)
 
 
 
     (BPhotographers bPhotographers) <- grab @BPhotographers
-    let what =
-            (   (fromMaybe [])
-            <$> (fmap (ListZipper.toList . Photographer.unPhotographers))
-            <$> bPhotographers
-            )
-    elemPhotographers <- liftUI $ UI.div # sink items what
+    let childs = (fmap lol
+                    <$> (fromMaybe [])
+                    <$> (fmap (ListZipper.toList . Photographer.unPhotographers))
+                    <$> bPhotographers
+                    )
+    elemPhotographers               <- liftUI $ UI.div # sink items' childs
 
-    liftUI $ getBody win #+ [element elem, element elemPhotographers]
+    liftUI
+        $  getBody win
+        #+ [ element elemToken
+     --      , element elemPhotographers
+           , UI.hr
+           , element elemTabs
+           , UI.hr
+           , element elemContent 
+           ]
     return ()
 
+lol :: Show a => a -> UI Element
+lol a =  UI.string (show a)
+
+items :: Show a => ReadWriteAttr Element [a] ()
 items = mkWriteAttr $ \is x -> void $ do
     return x # set children [] #+ fmap (\i -> UI.string (show i)) is
+
+items' :: ReadWriteAttr Element [UI Element] ()
+items' = mkWriteAttr $ \is x -> void $ do
+    is' <- sequence is
+    return x # set children is'
 
 
 getTabsClient = do
     (GetTabs getTabs) <- grab @GetTabs
-    (BToken           bToken          ) <- grab @BToken
-    (HTabs   hTabs ) <- grab @HTabs
-    token                               <- currentValue bToken
+    (BToken  bToken ) <- grab @BToken
+    (HTabs   hTabs  ) <- grab @HTabs
+    token             <- currentValue bToken
     case token of
         Nothing -> liftIO $ die "Missing token"
         Just t  -> do
